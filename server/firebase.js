a905ba645f7f7f526ba41390843ebcc15c0d7591
JonathanDre <132382491+JonathanDre@users.noreply.github.com>
2023-05-23 10:35:58 +0200

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -12,22 +12,6 @@ const db = getFirestore()
 
 module.exports = {db}
 
-
-/*import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyACpaOPnWR4d_ECO27BxkxIs09LArJmHVM",
-  authDomain: "online-registry.firebaseapp.com",
-  projectId: "online-registry",
-  storageBucket: "online-registry.appspot.com",
-  messagingSenderId: "421020583569",
-  appId: "1:421020583569:web:fcd03524e4871c35676130"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
